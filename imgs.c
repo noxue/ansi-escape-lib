7473f1183d7cabaa389ac83c21433925c62ff06b
noxue <yes@noxue.com>
2022-05-14 17:24:08 +0800

    添加了说明和效果图

---
+++ imgs.c
@@ -0,0 +1,107 @@
+#include "ansi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "imgs.h"
+#include <string.h>
+
+#define show_img(img)                                                    \
+    do                                                                   \
+    {                                                                    \
+        for (int i = 0; i < sizeof(img) / sizeof(img[0]); i++)           \
+        {                                                                \
+            for (int j = 0; j < sizeof(img[0]) / sizeof(img[0][0]); j++) \
+            {                                                            \
+                int *color = img[i][j];                                  \
+                ansi_fg_rgb(color[0], color[1], color[2]);               \
+                ansi_font_reverse();                                     \
+                printf("  ");                                            \
+            }                                                            \
+            printf("\n");                                                \
+        }                                                                \
+    } while (0)
+
+int main(int argc, char const *argv[])
+{
+
+    // 获取命令行参数
+    if (argc < 2)
+    {
+        printf("Usage: %s <img_name> [flash]\n", argv[0]);
+        printf("<img_name>:alipay, wx, android, huawei, linux, waner, wangzhe, weibo, win9, win11, windows, wx\n");
+        return 0;
+    }
+// 如果是windows平台
+#ifdef _WIN32
+    system("cls"); // 如果不清屏，ansi可能会不起作用
+#else              // linux
+    system("clear");
+#endif
+    ansi_reset();
+    ansi_font_bold();
+
+    // 如果设置了flash
+    if (argc == 3 && strcmp(argv[2], "flash") == 0)
+    {
+        ansi_font_flash();
+    }
+
+    const char *img = argv[1];
+    // 比较字符串
+
+    if (!strcmp(img, "alipay"))
+    {
+        show_img(img_alipay);
+    }
+    else if (!strcmp(img, "wx"))
+    {
+        show_img(img_wx);
+    }
+    else if (!strcmp(img, "android"))
+    {
+        show_img(img_android);
+    }
+    else if (!strcmp(img, "huawei"))
+    {
+        show_img(img_huawei);
+    }
+    else if (!strcmp(img, "linux"))
+    {
+        show_img(img_linux);
+    }
+    else if (!strcmp(img, "waner"))
+    {
+        show_img(img_waner);
+    }
+    else if (!strcmp(img, "wangzhe"))
+    {
+        show_img(img_wangzhe);
+    }
+    else if (!strcmp(img, "weibo"))
+    {
+        show_img(img_weibo);
+    }
+    else if (!strcmp(img, "win9"))
+    {
+        show_img(img_win9);
+    }
+    else if (!strcmp(img, "win11"))
+    {
+        show_img(img_win11);
+    }
+    else if (!strcmp(img, "windows"))
+    {
+        show_img(img_windows);
+    }
+    else if (!strcmp(img, "wx"))
+    {
+        show_img(img_wx);
+    }
+    else
+    {
+        printf("No such image: %s\n", img);
+    }
+
+    ansi_font_no_flash();
+    ansi_reset();
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# ansi-escape-lib
+## ansi-escape-lib
 
 ansi-escape-lib 是一个简单的 ansi escape 库，它可以用于在终端上打印 ansi escape 码，
 
@@ -6,8 +6,58 @@ ansi-escape-lib 是一个简单的 ansi escape 库，它可以用于在终端上
 
 ansi escape 封装，方便控制命令行颜色和光标
 
-# 使用方法
+## 使用方法
 
 把 ansi.h 和 ansi.c 复制到自己项目，并引入 ansi.h
 
 兼容 gcc 和 vs
+
+## 例子
+
+imgs.c 显示图片，imgs.h 图片头文件，保存了图片的 rgb 数组，可以直接使用
+
+## 图片转 c 语言 rgb 数组
+
+imgs.h 是使用这个项目 <https://github.com/noxue/img2rgb> 来生成的 c 语言数组
+
+- 编译命令
+
+  ```
+  gcc -o imgs.exe imgs.c ansi.c
+  ```
+
+- 运行命令
+
+  - 执行命令如果不带参数，会提示你可以输入哪些参数
+
+  ```
+  ./imgs.exe
+  ```
+
+  - 比如显示 qq 图片
+
+  ```
+  ./imgs.exe qq
+  ```
+
+### 效果图
+
+![](imgs/1.png)
+
+![](imgs/2.png)
+
+![](imgs/3.png)
+
+![](imgs/4.png)
+
+![](imgs/5.png)
+
+![](imgs/6.png)
+
+![](imgs/7.png)
+
+![](imgs/8.png)
+
+![](imgs/9.png)
+
+![](imgs/10.png)
